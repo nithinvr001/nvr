9bdf92a3d68fae90eeeb9d976fa1b162af74969d
nithinvr001 <nithinvramesh@outlook.com>
2025-07-11 17:01:59 +0530

    folioDone1.5

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,7 +6,7 @@ import { ReactTyped } from 'react-typed';
 export default function Hero() {
   return (
     <div>
-        <h1 className='text-white ml-[20px] pt-[20px] my-[50px] text-5xl'>NITHIN V R</h1>
+        <h1 className='text-white !font-serif ml-[20px] pt-[20px] my-[50px] text-5xl'>NITHIN V R</h1>
       <div className='lg:flex'>
           <img
             src={deskImage}
@@ -68,7 +68,7 @@ export default function Hero() {
             </div> */}
             <div className="mt-4 flex gap-4">
                 <a
-                  href="/resume.pdf"
+                  href="https://drive.google.com/file/d/1ZpkTMmteDxfM0opR_h2OmlSDpAIyxZk3/view?usp=sharing"
                   download
                   className="bg-accent text-black px-4 py-2 rounded-lg font-semibold hover:bg-green-400"
                 >
